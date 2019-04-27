15a469701ef7f14dae4546e1fb0d8399527c8c74
Federico 'Morg' Pareschi <morgawr@gmail.com>
2019-04-27 20:58:22 +0100

    Added Battle Scene and Enemy AI

---
+++ Assets/Scripts/EntityMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LD44 {
+public class EntityMover : MonoBehaviour {
+
+    Rigidbody2D body;
+
+    public float speed;
+
+    void Start() {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+        body.transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0), Space.Self);
+    }
+}
+}
---
+++ Assets/Scripts/SpriteTilter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LD44 {
+public class SpriteTilter : MonoBehaviour {
+
+    public int wobbleIntensity = 10;
+
+    public float duration = 30;
+    public float startOffset = 0;
+    public float startTime = 0f;
+
+    public Vector3 PivotPoint = Vector3.zero;
+
+    void Start() {
+        startTime = Time.time;
+    }
+
+    void OnDisable() {
+        SetZAngle(0);
+    }
+
+    void FixedUpdate() {
+        var timeDiff = (Time.time - startTime) * 1000;
+        if(startOffset > timeDiff)
+            return;
+		float value = Mathf.Sin((((timeDiff - startOffset) % duration) / duration) * Mathf.PI * 2);
+        SetZAngle(wobbleIntensity * value);
+    }
+
+    void SetZAngle(float value) {
+        var z_value = transform.localRotation.eulerAngles.z;
+        transform.RotateAround(transform.TransformPoint(PivotPoint), transform.forward, value - z_value);
+    }
+}
+}
