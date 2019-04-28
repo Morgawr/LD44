0c9384ee5bff5d44afcb98bad1908c3d1358d4b8
Federico 'Morg' Pareschi <morgawr@gmail.com>
2019-04-28 14:31:38 +0100

    Added dragon cave, citadel, and castle scenes

---
+++ Assets/Scripts/EventHandlers/DragonsLairClickHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+namespace LD44 {
+public class DragonsLairClickHandler : MonoBehaviour, IPointerClickHandler {
+    public void OnPointerClick(PointerEventData data) {
+        if(data.button == PointerEventData.InputButton.Left) {
+            SceneManager.UnloadSceneAsync("MapScene");
+            SceneManager.LoadSceneAsync("DragonCaveScene", LoadSceneMode.Additive);
+        }
+    }
+}
+}
